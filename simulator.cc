d6ed3f5ec8270de68af2a74c25c7038c990ec791
Xi Luo <dycz0fx@gmail.com>
2020-09-24 01:42:24 -0400

    add inter-node communication

--- simulator.cc
+++ simulator.cc
@@ -1,22 +1,79 @@
 #include "simulator.h"
 
+using std::string;
+using std::vector;
+using std::cout;
+using std::endl;
+using std::to_string;
+using std::unordered_map;
+using std::max;
+
 // class Device
-Device::Device(string name, DeviceType type)
-: name(name), type(type)
+Device::Device(string name, DeviceType type, int id)
+: name(name), type(type), id(id)
 {
 }
 
 // class Comp_device
-Comp_device::Comp_device(string name)
-: Device(name, Device::DEVICE_COMP)
+Comp_device::Comp_device(string name, int id)
+: Device(name, Device::DEVICE_COMP, id)
 {
 }
 
 // class Comm_device
-Comm_device::Comm_device(string name, float latency, float bandwidth)
-: Device(name, Device::DEVICE_COMM), latency(latency), bandwidth(bandwidth)
+Comm_device::Comm_device(string name, int id, float latency, float bandwidth)
+: Device(name, Device::DEVICE_COMM, id), latency(latency), bandwidth(bandwidth)
 {
 }
+
+// class Machine
+Machine::Machine() {
+    comp_to_dram.clear();
+    comp_to_nic_in.clear();
+    comp_to_nic_out.clear();
+}
+
+void Machine::attach_dram(Comp_device *comp, Comm_device *comm)
+{
+    assert(starts_with(comm->name, "DRAM"));
+    if (comp_to_dram.find(comp->id) == comp_to_dram.end()) {
+        comp_to_dram[comp->id] = comm;
+    }
+}
+
+void Machine::attach_nic_in(Comp_device *comp, Comm_device *comm)
+{
+    assert(starts_with(comm->name, "NIC"));
+    if (comp_to_nic_in.find(comp->id) == comp_to_nic_in.end()) {
+        comp_to_nic_in[comp->id] = comm;
+    }
+}
+
+void Machine::attach_nic_out(Comp_device *comp, Comm_device *comm)
+{
+    assert(starts_with(comm->name, "NIC"));
+    if (comp_to_nic_out.find(comp->id) == comp_to_nic_out.end()) {
+        comp_to_nic_out[comp->id] = comm;
+    }
+}
+
+vector<Comm_device *> Machine::get_comm_path(Comp_device *source, Comp_device *target)
+{
+    vector<Comm_device *> ret;
+    // on the same node
+    if (source->id == target->id) {
+        ret.emplace_back(comp_to_dram[source->id]);
+    }
+    // on different nodes
+    else {
+        ret.emplace_back(comp_to_dram[source->id]);
+        ret.emplace_back(comp_to_nic_out[source->id]);
+        ret.emplace_back(comp_to_nic_in[target->id]);
+        ret.emplace_back(comp_to_dram[target->id]);
+    }
+    return ret;
+}
+
 
 // class Task
 Task::Task(string name, Device *device) 
@@ -32,7 +89,7 @@ void Task::add_next_task(Task *task)
 }
 
 // class Comp_task
-Comp_task::Comp_task(string name, Device *device, float run_time)
+Comp_task::Comp_task(string name, Comp_device *device, float run_time)
 : Task(name, device), run_time(run_time)
 {
 }
@@ -43,7 +100,7 @@ float Comp_task::cost()
 }
 
 // class Comm_task
-Comm_task::Comm_task(string name, Device *device, int message_size)
+Comm_task::Comm_task(string name, Comm_device *device, int message_size)
 : Task(name, device), message_size(message_size)
 {
 }
@@ -55,47 +112,71 @@ float Comm_task::cost()
 }
 
 // class Simulator
-Task *Simulator::new_comp_task(vector<Task *> prev_tasks, string name, Device *device, float run_time)
+Task *Simulator::new_comp_task(string name, Comp_device *device, float run_time)
 {
     Task *cur_task = (Task *) new Comp_task(name, device, run_time);
-    if (prev_tasks.empty()) {
-        ready_queue.push(cur_task);
-    }
-    else {
-        for (size_t i = 0; i < prev_tasks.size(); i++) {
-            prev_tasks[i]->add_next_task(cur_task);
-        }
-    }
     return cur_task;
 }
 
-vector<Task *> Simulator::new_comm_task(vector<Task *> prev_tasks, string name, Device *device, int message_size)
+std::vector<Task *> Simulator::new_comm_task(Task *source_task, Task *target_task, int message_size)
 {
-    vector<Task *> ret_tasks;
-    // Divide messages into segments
+    vector<Comm_device *> path = machine.get_comm_path((Comp_device *)(source_task->device), (Comp_device *)(target_task->device));
+    vector<vector<Task *>> all_tasks;
     int num_segment = message_size / SEG_SIZE;
-    int seg_size = SEG_SIZE;
-    for (int i = 0; i < num_segment; i++) {
-        if (i == num_segment - 1) {
-            seg_size = message_size - (num_segment - 1) * SEG_SIZE;
+    // Create all the comm tasks
+    // Divide messages into segments
+    for (int i = 0; i < path.size(); i++) {
+        all_tasks.push_back({});
+        for (int j = 0; j < num_segment; j++) {
+            int seg_size = SEG_SIZE;
+            if (j == num_segment - 1) {
+                seg_size = message_size - (num_segment - 1) * SEG_SIZE;
+            }
+            string name = "seg " + to_string(j) + " from " + source_task->name + " to " + target_task->name;
+            Task *cur_task = (Task *) new Comm_task(name, path[i], seg_size);
+            all_tasks[i].push_back(cur_task);
         }
-        Task *cur_task = (Task *) new Comm_task(name, device, seg_size);
-        if (prev_tasks.empty()) {
-            ready_queue.push(cur_task);
-        }
-        else {
-            for (size_t i = 0; i < prev_tasks.size(); i++) {
-                prev_tasks[i]->add_next_task(cur_task);
-            }        
-        }
-        ret_tasks.push_back(cur_task);
     }
-    return ret_tasks;
+
+    // Add dependencies among the comm tasks
+    for (int i = 0; i < path.size(); i++) {
+        for (int j = 0; j < num_segment; j++) {
+            if (i == 0) {
+                add_dependency(source_task, all_tasks[i][j]);
+            }
+            if (i == path.size() - 1) {
+                add_dependency(all_tasks[i][j], target_task);
+            }
+            if (i > 0) {
+                add_dependency(all_tasks[i-1][j], all_tasks[i][j]);
+            }
+        }
+    }
+
+    return all_tasks.back();
+}
+
+void Simulator::enter_ready_queue(Task *task)
+{
+    ready_queue.push(task);
+}
+
+void Simulator::add_dependency(vector<Task *> prev_tasks, Task *cur_task)
+{
+    for (int i = 0; i < prev_tasks.size(); i++) {
+        prev_tasks[i]->add_next_task(cur_task);
+    }
+    
+}
+
+void Simulator::add_dependency(Task *prev_task, Task *cur_task)
+{
+    prev_task->add_next_task(cur_task);
 }
 
 void Simulator::simulate()
 {
-    srand (time(NULL));
+    srand(time(NULL));
     float sim_time = 0.0f;
     unordered_map<Device*, float> device_times;
     while (!ready_queue.empty()) {
@@ -116,7 +197,7 @@ void Simulator::simulate()
         }
         float end_time = start_time + run_time;
         device_times[cur_task->device] = end_time;
-        cout << cur_task->name << " on " << cur_task->device->name << " --- " << "ready(" << 
+        cout << cur_task->name << " --- " << cur_task->device->name << " --- " << "ready(" << 
             ready_time << ") start("  << start_time << ") run(" << run_time << ") end(" <<  end_time << ")" << endl;
         if (end_time > sim_time)
             sim_time = end_time;
@@ -136,54 +217,90 @@ void Simulator::simulate()
 
 int main()
 {
-    int size = 4;
-    vector<Comp_device> cpus;
-    for (int i = 0; i < size; i++){
-        string cpu_name = "CPU" + to_string(i);
-        cpus.emplace_back(Comp_device(cpu_name));
-    }
-    Comm_device dram("DRAM", 0, 1); // ms, B/ms
-
     Simulator simulator;
-    // 0 1 2 3
-    //  / / /
-    // 0 1 2 3
-    // 0st iteration
-    vector<Task *> comp_tasks_0;
-    for (int i = 0; i < size; i++){
-        string task_name = "[0] compute " + to_string(i);
-        int run_time = 10;
-        comp_tasks_0.emplace_back(simulator.new_comp_task({}, task_name, (Device *)(&cpus[i]), run_time));
+
+    int num_tasks = 4;
+    int num_nodes = 2;
+    int num_tasks_per_node = num_tasks / num_nodes;
+
+    vector<vector<Comp_device *>> cpus;
+    for (int i = 0; i < num_nodes; i++) {
+        cpus.push_back({});
+        for (int j = 0; j < num_tasks_per_node; j++) {
+            string cpu_name = "CPU " + to_string(j) + " on NODE " + to_string(i);
+            cpus[i].emplace_back(new Comp_device(cpu_name, i));
+        }
     }
 
-    // communication
-    unordered_map<int, vector<Task *>> comm_tasks_0;
-    for (int i = 0; i < size; i++){
+    for (int i = 0; i < num_nodes; i++) {
+        string dram_name = "DRAM " + to_string(i) + " on NODE " + to_string(i);
+        Comm_device *dram = new Comm_device(dram_name, i, 0, 1); // ms, B/ms or kB/s
+        // drams.emplace_back(Comm_device(dram_name, i, 0.00003, 8.75 * 1024 * 1024)); // ms, B/ms or kB/s
+        string nic_in_name = "NIC_IN " + to_string(i) + " on NODE " + to_string(i);
+        Comm_device *nic_in = new Comm_device(nic_in_name, i, 0, 2);
+        // nics_in.emplace_back(Comm_device(nic_in_name, i, 0.006214, 5 * 1024 * 1024));
+        string nic_out_name = "NIC_OUT " + to_string(i) + " on NODE " + to_string(i);
+        Comm_device *nic_out = new Comm_device(nic_out_name, i, 0, 2);
+        // nics_out.emplace_back(Comm_device(nic_out_name, i, 0.006214, 5 * 1024 * 1024));
+        simulator.machine.attach_dram(cpus[i][0], dram);
+        simulator.machine.attach_nic_in(cpus[i][0], nic_in);
+        simulator.machine.attach_nic_out(cpus[i][0], nic_out);
+    }
+
+    // 1D stencil
+    // 0: 0 1
+    // 1: 0 1 2
+    // 2: 1 2 3
+    // 3: 2 3
+
+    // init comp tasks first
+    // iteration 0
+    vector<Task *> comp_tasks_0;
+    for (int i = 0; i < num_nodes; i++) {
+        for (int j = 0; j < num_tasks_per_node; j++) {
+            int k = i * num_tasks_per_node + j;
+            string task_name = "[0] compute " + to_string(k);
+            int run_time = 10;
+            comp_tasks_0.emplace_back(simulator.new_comp_task(task_name, cpus[i][j], run_time));
+        }
+    }
+
+    for (int i = 0; i < comp_tasks_0.size(); i++) {
+        simulator.enter_ready_queue((Task *)comp_tasks_0[i]);
+    }
+
+    // iteration 1
+    vector<Task *> comp_tasks_1;
+    for (int i = 0; i < num_nodes; i++) {
+        for (int j = 0; j < num_tasks_per_node; j++) {
+            int k = i * num_tasks_per_node + j;        
+            string task_name = "[1] compute " + to_string(k);
+            int run_time = 10;
+            comp_tasks_1.emplace_back(simulator.new_comp_task(task_name, cpus[i][j], run_time));
+        }
+    }
+
+    // add comm tasks between iter 0 and iter 1
+    vector<Task *> comm_tasks_0;
+    for (int i = 0; i < num_tasks; i++){
+        int message_size = 10;
+        // left
         int left = i - 1;
         if (left >= 0) {
-            string task_name = "[0] send from " + to_string(i) + " to " + to_string(left);
-            vector<Task *> prev_tasks;
-            prev_tasks.emplace_back(comp_tasks_0[i]);
-            vector<Task *> comm_tasks = simulator.new_comm_task(prev_tasks, task_name, (Device *)&dram, 10);
-            comm_tasks_0[left].insert(comm_tasks_0[left].end(), comm_tasks.begin(), comm_tasks.end());
+            vector<Task *> comm_tasks = simulator.new_comm_task(comp_tasks_0[i], comp_tasks_1[left], 10);
+            comm_tasks_0.insert(comm_tasks_0.end(), comm_tasks.begin(), comm_tasks.end());
         }
+        /*
+        // right
         int right = i + 1;
-        if (right < size) {
-            string task_name = "[0] send from " + to_string(i) + " to " + to_string(right);
-            vector<Task *> prev_tasks;
-            prev_tasks.emplace_back(comp_tasks_0[i]);
-            vector<Task *> comm_tasks = simulator.new_comm_task(prev_tasks, task_name, (Device *)&dram, 10);
-            comm_tasks_0[right].insert(comm_tasks_0[right].end(), comm_tasks.begin(), comm_tasks.end());
+        if (right < num_tasks) {
+            vector<Task *> comm_tasks = simulator.new_comm_task(comp_tasks_0[i], comp_tasks_1[right], 10);
+            comm_tasks_0.insert(comm_tasks_0.end(), comm_tasks.begin(), comm_tasks.end());
         }
-    }
-
-    // 1st iteration
-    vector<Task *> comp_tasks_1;
-    for (int i = 0; i < size; i++){
-        string task_name = "[1] compute " + to_string(i);
-        vector<Task *> prev_tasks;
-        prev_tasks.insert(prev_tasks.end(), comm_tasks_0[i].begin(), comm_tasks_0[i].end());
-        comp_tasks_1.emplace_back(simulator.new_comp_task(prev_tasks, task_name, (Device *)(&cpus[i]), 10));
+        */
+        // mid
+        vector<Task *> comm_tasks = simulator.new_comm_task(comp_tasks_0[i], comp_tasks_1[i], 10);
+        comm_tasks_0.insert(comm_tasks_0.end(), comm_tasks.begin(), comm_tasks.end());
     }
 
     simulator.simulate();
--- simulator.h
+++ simulator.h
@@ -5,9 +5,7 @@
 #include <string>
 #include <time.h>
 
-using namespace std;
-
-#define SEG_SIZE 5
+#define SEG_SIZE 10
 
 class Device
 {
@@ -16,15 +14,16 @@ public:
         DEVICE_COMP,
         DEVICE_COMM,
     };
-    Device(string name, DeviceType type);
-    string name;
+    Device(std::string name, DeviceType type, int id);
+    std::string name;
     DeviceType type;
+    int id;
 };
 
 class Comp_device : public Device
 {
 public:
-    Comp_device(string name);
+    Comp_device(std::string name, int id);
 };
 
 class Comm_device : public Device
@@ -32,19 +31,31 @@ class Comm_device : public Device
 public:
     float latency;
     float bandwidth;
-    Comm_device(string name, float latency, float bandwidth);
+    Comm_device(std::string name, int id, float latency, float bandwidth);
+};
+
+class Machine
+{
+private:
+    std::unordered_map<int, Comm_device *> comp_to_dram;
+    std::unordered_map<int, Comm_device *> comp_to_nic_in;
+    std::unordered_map<int, Comm_device *> comp_to_nic_out;
+public:
+    Machine();
+    void attach_dram(Comp_device *comp, Comm_device *comm);
+    void attach_nic_in(Comp_device *comp, Comm_device *comm);
+    void attach_nic_out(Comp_device *comp, Comm_device *comm);
+    std::vector<Comm_device *> get_comm_path(Comp_device *source, Comp_device *target);
 };
 
 class Task
 {
-private:
-    /* data */
 public:
-    Task(string name, Device *device);
-    string name;
+    Task(std::string name, Device *device);
+    std::string name;
     Device *device;
     float ready_time;
-    vector<Task *> next_tasks;
+    std::vector<Task *> next_tasks;
     int counter;
     void add_next_task(Task *task);
 };
@@ -52,7 +63,7 @@ public:
 class Comp_task : public Task
 {
 public:
-    Comp_task(string name, Device *device, float run_time);
+    Comp_task(std::string name, Comp_device *device, float run_time);
     float run_time;
     float cost();
 };
@@ -60,7 +71,7 @@ public:
 class Comm_task : public Task
 {
 public:
-    Comm_task(string name, Device *device, int message_size);
+    Comm_task(std::string name, Comm_device *device, int message_size);
     int message_size;
     float cost();
 };
@@ -78,9 +89,18 @@ public:
 class Simulator
 {
 private:
-    priority_queue<Task *, vector<Task *>, TaskCompare> ready_queue;
+    std::priority_queue<Task *, std::vector<Task *>, TaskCompare> ready_queue;
 public:
-    Task *new_comp_task(vector<Task *> prev_tasks, string name, Device *device, float run_time);
-    vector<Task *> new_comm_task(vector<Task *> prev_tasks, string name, Device *device, int message_size);
+    Machine machine;
+    Task *new_comp_task(std::string name, Comp_device *device, float run_time);
+    // new_comm_task returns the comm_tasks in the last phase 
+    std::vector<Task *> new_comm_task(Task *source_task, Task *target_task, int message_size);
+    void enter_ready_queue(Task *task);
+    void add_dependency(std::vector<Task *> prev_tasks, Task *cur_task);
+    void add_dependency(Task *prev_task, Task *cur_task);
     void simulate();
 };
+
+bool starts_with(std::string s, std::string sub){
+    return s.find(sub) == 0 ? 1 : 0;
+}
